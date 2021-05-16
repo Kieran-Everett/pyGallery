b83b1eb52e7d8fbbd214a64d25aff9fb6e6d3fc8
Kieran Everett <kieraneverett03@gmail.com>
2021-05-16 23:18:24 +0100

    Created the gallery database

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import os
+from datetime import datetime
 
 from flask import Flask, render_template, request, flash
 from flask_uploads import IMAGES, UploadSet, configure_uploads
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gallery.db'
+db = SQLAlchemy(app)
+
 photos = UploadSet('photos', IMAGES)
 app.config['UPLOADED_PHOTOS_DEST'] = 'data/'
 app.config['SECRET_KEY'] = os.urandom(24)
 configure_uploads(app, photos)
+
+
+class Gallery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    picName = db.Column(db.String(100), nullable=False, unique=True)
+    tags = db.Column(db.String())
+    dateUploaded = db.Column(db.DateTime, default=datetime.utcnow)
+    uploadedBy = db.Column(db.String(40))
+
+    def __repr__(self):
+        return '<Picture %r>' % self.id
 
 
 @app.route('/')
