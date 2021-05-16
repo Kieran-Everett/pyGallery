4e45113e041b85d3f72e1538dbc5531198d583fb
Kieran Everett <kieraneverett03@gmail.com>
2021-05-16 19:39:54 +0100

    Added uploading

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, flash
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'data/'
+app.config['SECRET_KEY'] = os.urandom(24)
+configure_uploads(app, photos)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        photos.save(request.files['photo'])
+        flash('Photo saved successfully')
+        return render_template('upload.html')
+    return render_template('upload.html')
 
 
 if __name__ == "__main__":
